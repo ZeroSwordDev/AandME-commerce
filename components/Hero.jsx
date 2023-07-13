32d22ba59c825cd748e84819b5edb904515227d0
ZeroSwordDev <miguelworking1@gmail.com>
2023-07-13 15:45:55 -0400

    Cambios comprar en 5 pasos

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <section id="hero" className="   max-w-[1440px] h-screen mx-auto mt-4 px-2">
-      <div className="relative w-full overflow-hidden left-0 h-[340px] rounded-3xl   ">
+    <section id="hero" className="max-w-[1440px] h-screen mx-auto mt-4 px-2">
+      <article className="relative w-full overflow-hidden left-0 h-[340px] rounded-3xl   ">
         <div className=" absolute w-full h-full bg-black/60 rounded-3xl"></div>
 
         <div className="absolute top-[20%] left-5 space-y-2 md:left-14 lg:left-20">
@@ -19,10 +19,10 @@ const Hero = () => {
           src="https://cdn.pixabay.com/photo/2016/11/13/22/17/print-1822209_1280.jpg"
           alt=""
         />
-      </div>
+      </article>
       {/* Categories */}
       <div className=" flex flex-wrap  h-[700px]  items-center  w-full gap-2   lg:h-[230px]  mt-2 ">
-        <div className="relative  flex-grow overflow-hidden h-52 w-[460px] rounded-xl  ">
+        <article className="relative  flex-grow overflow-hidden h-52 w-[460px] rounded-xl  ">
           {/* overlay */}
           <div className="absolute bg-purple-900/60  w-full h-full"></div>
 
@@ -41,9 +41,9 @@ const Hero = () => {
             src="https://images.pexels.com/photos/10458835/pexels-photo-10458835.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
           />
-        </div>
+        </article>
 
-        <div className="relative flex-grow h-52 w-[460px] overflow-hidden rounded-xl  ">
+        <article className="relative flex-grow h-52 w-[460px] overflow-hidden rounded-xl  ">
           <div className="absolute bg-red-800/60  w-full h-full"></div>
 
           <div className="absolute top-[20%] left-10">
@@ -62,9 +62,9 @@ const Hero = () => {
             src="https://images.pexels.com/photos/3194518/pexels-photo-3194518.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
           />
-        </div>
+        </article>
 
-        <div className="relative flex-grow h-52 w-[460px] overflow-hidden rounded-xl ">
+        <article className="relative flex-grow h-52 w-[460px] overflow-hidden rounded-xl ">
           <div className=" absolute top-0 bg-blue-900/50  w-full h-full"></div>
           <div className="absolute top-[20%] left-10">
             <h1 className="text-5xl text-white font-semibold">Tarjeteria</h1>
@@ -80,8 +80,39 @@ const Hero = () => {
             src="https://images.pexels.com/photos/6224/hands-people-woman-working.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
             alt=""
           />
-        </div>
+        </article>
       </div>
+      {/* Section  */}
+        <article className=" flex flex-wrap justify-between items-center w-full h-52">
+          <div className="flex items-center px-4 h-52 w-52 ">
+            <h2 className="text-3xl">Realiza tu Compra en <strong>5 sencillos pasos</strong></h2>
+          </div>
+          <hr  className=" w-[1px] h-[120px] border-black/50 border"/>
+          <div className="flex flex-col space-y-2 items-center justify-center px-4 h-52 w-52 ">
+            <img src="/addproduct.png" alt="" />
+            <h2 className="text-2xl text-center"> Elige el producto al carrito</h2>
+          </div>
+         <hr  className=" w-[1px] h-[120px] border-black/50 border"/>
+          <div className="flex flex-col space-y-2 justify-center text-center items-center px-4 h-52 w-52 ">
+            <img src="/desing.png" alt="" className="w-16 h-16" />
+            <h2 className="text-2xl">Sube tu diseño</h2>
+          </div>
+          <hr  className=" w-[1px] h-[120px] border-black/50 border"/>
+          <div className="flex flex-col text-center justify-center space-y-2 items-center px-4 h-52 w-52 ">
+            <img src="/addcart.png" alt="" className="w-16 h-16" />
+            <h2 className="text-2xl">Agrega al carrito</h2>
+          </div>
+         <hr  className=" w-[1px] h-[120px] border-black/50 border"/>
+          <div className="flex flex-col justify-center space-y-2 text-center items-center px-4 h-52 w-52 ">
+            <img src="/payment.png" alt=""  className="w-16 h-16"/>
+            <h2 className="text-2xl">Paga en linea</h2>
+          </div>
+         <hr  className=" w-[1px] h-[120px] border-black/50 border"/>
+          <div className="flex flex-col space-y-2 justify-center text-center items-center px-4 h-52 w-52 ">
+            <img src="/recivedProduct.png" alt=""  className="w-20 h-20"/>
+            <h2 className="text-2xl">Retira o recibe tu producto</h2>
+          </div>
+        </article>
     </section>
   );
 };
